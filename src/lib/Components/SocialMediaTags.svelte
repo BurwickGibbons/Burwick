54bb0a2d5df23962bcf53afd5c1e25752db1f207
Reuben Gibbons <reuben.gibbons@me.com>
2022-07-02 22:18:03 +0100

    update

--- src/lib/Components/SocialMediaTags.svelte
+++ src/lib/Components/SocialMediaTags.svelte
@@ -1,26 +1,26 @@
 <script>
 	let size =55;
 	let pad = 5;
-	let border = 4;
+	let height = 2;
 </script>
 
 <div class="
-max-w-screen-sm 
-mx-auto 
-border-ridge
-border-r-{border}
-border-l-{border}
-border-t-{border}
-rounded-tr-full 
-rounded-tl-full
-bg-slate-200
-border-slate-700
-
+max-w-screen-sm
+mx-auto
+sm:opacity-0
+md:opacity-100
 ">
 
     <div class="flex items-center justify-center">
-	<a href="https://www.instagram.com/reuben.gibbons/?hl=en" class="p-{pad}">
-		<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 3364.7 3364.7" width={size} height={size}
+	<a 
+		href="https://www.instagram.com/reuben.gibbons/?hl=en"
+		class="p-{pad} hover:-translate-y-{height}"
+		>
+		<svg 
+			xmlns="http://www.w3.org/2000/svg" 
+			viewBox="0 0 3364.7 3364.7" 
+			width={size} 
+			height={size}
 			><defs
 				><radialGradient id="0" cx="217.76" cy="3290.99" r="4271.92" gradientUnits="userSpaceOnUse"
 					><stop offset=".09" stop-color="#fa8f21" /><stop
@@ -48,9 +48,13 @@ border-slate-700
 
 	<a
 		href="https://uk.linkedin.com/in/reuben-gibbons-5426a4149?trk=people-guest_people_search-card"
-		class="p-{pad}"
+		class="p-{pad} hover:-translate-y-{height}"
 		>
-		<svg height={size} width={size} xmlns="http://www.w3.org/2000/svg" viewBox="0 0 256 256"
+		<svg 
+			height={size}
+			width={size} 
+			xmlns="http://www.w3.org/2000/svg" 
+			viewBox="0 0 256 256"
 			><g fill="none"
 				><path
 					d="M0 18.338C0 8.216 8.474 0 18.92 0h218.16C247.53 0 256 8.216 256 18.338v219.327C256 247.79 247.53 256 237.08 256H18.92C8.475 256 0 247.791 0 237.668V18.335z"
@@ -63,7 +67,10 @@ border-slate-700
 		>
 	</a>
 
-	<a href="https://www.facebook.com/reuben.gibbons.37/" class="p-{pad}">
+	<a 	
+		href="https://www.facebook.com/reuben.gibbons.37/" 
+		class="p-{pad} hover:-translate-y-{height}"
+		>
 		<svg
 			xmlns="http://www.w3.org/2000/svg"
 			width={size}
@@ -75,13 +82,37 @@ border-slate-700
 			/></svg
 		>
 	</a>
-    <a href="https://github.com/BurwickGibbons" class="p-{pad} hover:animate-bounce">
-        <svg height={size} width= {size} role="img" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><title>GitHub</title><path d="M12 .297c-6.63 0-12 5.373-12 12 0 5.303 3.438 9.8 8.205 11.385.6.113.82-.258.82-.577 0-.285-.01-1.04-.015-2.04-3.338.724-4.042-1.61-4.042-1.61C4.422 18.07 3.633 17.7 3.633 17.7c-1.087-.744.084-.729.084-.729 1.205.084 1.838 1.236 1.838 1.236 1.07 1.835 2.809 1.305 3.495.998.108-.776.417-1.305.76-1.605-2.665-.3-5.466-1.332-5.466-5.93 0-1.31.465-2.38 1.235-3.22-.135-.303-.54-1.523.105-3.176 0 0 1.005-.322 3.3 1.23.96-.267 1.98-.399 3-.405 1.02.006 2.04.138 3 .405 2.28-1.552 3.285-1.23 3.285-1.23.645 1.653.24 2.873.12 3.176.765.84 1.23 1.91 1.23 3.22 0 4.61-2.805 5.625-5.475 5.92.42.36.81 1.096.81 2.22 0 1.606-.015 2.896-.015 3.286 0 .315.21.69.825.57C20.565 22.092 24 17.592 24 12.297c0-6.627-5.373-12-12-12"/></svg>
+    <a 	
+		href="https://github.com/BurwickGibbons"
+		class="p-{pad} hover:-translate-y-{height}"
+		>
+        <svg 
+			height={size} 
+			width= {size} 
+			role="img" 
+			viewBox="0 0 24 24" 
+			xmlns="http://www.w3.org/2000/svg"><title>GitHub</title><path d="M12 .297c-6.63 0-12 5.373-12 12 0 5.303 3.438 9.8 8.205 11.385.6.113.82-.258.82-.577 0-.285-.01-1.04-.015-2.04-3.338.724-4.042-1.61-4.042-1.61C4.422 18.07 3.633 17.7 3.633 17.7c-1.087-.744.084-.729.084-.729 1.205.084 1.838 1.236 1.838 1.236 1.07 1.835 2.809 1.305 3.495.998.108-.776.417-1.305.76-1.605-2.665-.3-5.466-1.332-5.466-5.93 0-1.31.465-2.38 1.235-3.22-.135-.303-.54-1.523.105-3.176 0 0 1.005-.322 3.3 1.23.96-.267 1.98-.399 3-.405 1.02.006 2.04.138 3 .405 2.28-1.552 3.285-1.23 3.285-1.23.645 1.653.24 2.873.12 3.176.765.84 1.23 1.91 1.23 3.22 0 4.61-2.805 5.625-5.475 5.92.42.36.81 1.096.81 2.22 0 1.606-.015 2.896-.015 3.286 0 .315.21.69.825.57C20.565 22.092 24 17.592 24 12.297c0-6.627-5.373-12-12-12"/></svg>
     </a>
 
     </div>
 </div>
 
 <style lang="postcss">
+
+	div{
 	
+		@apply rounded-t-full;
+		@apply border-t-2;
+		@apply border-l-2;
+		@apply border-r-2;
+	
+		@apply bg-slate-200;
+		@apply border-slate-700;
+	}
+
+	a{
+		transition: 1s;
+		}
+
+
 </style>
